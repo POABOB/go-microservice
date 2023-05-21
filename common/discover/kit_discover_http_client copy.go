27aa123376b919d26aea0a909e8e52458fcb7553
POABOB <zxc752166@yahoo.com.tw>
2023-05-21 09:49:55 +0800

    修改http和grpc成兩個檔案

--- common/discover/kit_discover_http_client copy.go
+++ common/discover/kit_discover_http_client copy.go
@@ -19,7 +19,7 @@ type KitDiscoverClient struct {
 	instancesMap sync.Map    // 暫存服務實例的資訊
 }
 
-func NewKitDiscoverClient(consulHost string, consulPort int) (DiscoveryClient, error) {
+func NewKitHTTPDiscoverClient(consulHost string, consulPort int) (DiscoveryClient, error) {
 	// 1. 設定Config和地址
 	consulConfig := api.DefaultConfig()
 	consulConfig.Address = consulHost + ":" + strconv.Itoa(consulPort)
@@ -53,8 +53,9 @@ func (consulClient *KitDiscoverClient) RegisterHTTP(serviceName, instanceId, hea
 	})
 }
 
-func (consulClient *KitDiscoverClient) RegisterGRPC(serviceName, instanceId, healthCheckUrl string, instanceHost string, instancePort int, meta map[string]string, logger *log.Logger) bool {
-	return consulClient.Register(&api.AgentServiceRegistration{
+func (consulClient *KitDiscoverClient) Register(serviceName, instanceId, healthCheckUrl string, instanceHost string, instancePort int, meta map[string]string, logger *log.Logger) bool {
+	// 服務實例的MetaData，呼叫註冊函數
+	err := consulClient.client.Register(&api.AgentServiceRegistration{
 		ID:      instanceId,
 		Name:    serviceName,
 		Address: instanceHost,
@@ -62,20 +63,15 @@ func (consulClient *KitDiscoverClient) RegisterGRPC(serviceName, instanceId, hea
 		Meta:    meta,
 		Check: &api.AgentServiceCheck{
 			DeregisterCriticalServiceAfter: "30s",
-			GRPC:                           instanceHost + ":" + strconv.Itoa(instancePort) + healthCheckUrl,
+			HTTP:                           "http://" + instanceHost + ":" + strconv.Itoa(instancePort) + healthCheckUrl,
 			Interval:                       "15s",
 		},
 	})
-}
-
-func (consulClient *KitDiscoverClient) Register(config *api.AgentServiceRegistration) bool {
-	// 服務實例的MetaData，呼叫註冊函數
-	err := consulClient.client.Register(config)
 	if err != nil {
-		log.Printf("Register Service: %s Error, instanceId %s \n", config.serviceName, config.instanceId)
+		log.Printf("Register Service: %s Error, instanceId %s \n", serviceName, instanceId)
 		return false
 	}
-	log.Printf("Register Service: %s Success, instanceId %s \n", config.serviceName, config.instanceId)
+	log.Printf("Register Service: %s Success, instanceId %s \n", serviceName, instanceId)
 	return true
 }
 
